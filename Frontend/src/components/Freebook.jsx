6e9739f423df0422f006a13f5a30bed08f68526e
SamiraJabin <127785519+SamiraJabin@users.noreply.github.com>
2024-12-18 13:17:17 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -11,7 +11,7 @@ function Freebook() {
   useEffect(()=>{
     const getBook = async()=>{
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstoreapp-backend-q345.onrender.com");
         const data = res.data.filter((data) => data.category === "Free");
         setBook(data);
       } catch (error) {
