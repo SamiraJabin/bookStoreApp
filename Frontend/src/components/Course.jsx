fec6ee99cc5f00d092a336c76012a8e17178a9f7
SamiraJabin <127785519+SamiraJabin@users.noreply.github.com>
2024-12-18 13:17:44 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -8,7 +8,7 @@ function Course() {
   useEffect(()=>{
     const getBook = async()=>{
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstoreapp-backend-q345.onrender.com");
         console.log(res.data);
         setBook(res.data)
       } catch (error) {
